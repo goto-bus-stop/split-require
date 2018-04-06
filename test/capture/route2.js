b4786c66390afc8a2e3fc4a9153311a0ef5b1449
Renée Kooi <renee@kooi.me>
2018-04-06 14:16:24 +0200

    make sure both capture test branches yield to the event loop for a full tick

--- test/capture/route2.js
+++ test/capture/route2.js
@@ -4,7 +4,8 @@ module.exports = function (cb) {
     Promise.resolve().then(function () {
       parallel([
         function (cb) { require('../../')('./view2', cb) },
-        function (cb) { require('../../')('./data3', cb) }
+        function (cb) { require('../../')('./data3', cb) },
+        function (cb) { setTimeout(cb, 1) } // ensure that there is an event loop yield at some point
       ], function (err, xyz) {
         cb(null, xyz[0](`Second route. ${xyz[1]}`))
       })
