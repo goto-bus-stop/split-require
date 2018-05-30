af83614e754afd2446ec28be833e8a3e0d64ae7e
Renée Kooi <renee@kooi.me>
2018-05-30 11:54:15 +0200

    Update expected browser-pack preludes in tests

--- test/name/expected/8e07f19b16.js
+++ test/name/expected/8e07f19b16.js
@@ -1,11 +1,6 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"entry3":[function(require,module,exports){
 require("split-require").l(3, require("a"));
-},{"a":3,"split-require":"split-require"}],3:[function(require,module,exports){
-var splitRequire = require('split-require')
+},{"a":3,"split-require":1}],3:[function(require,module,exports){
+module.exports = 'this will generate a hash'
 
-splitRequire(5, function (err, exports) {
-  console.log('d', exports)
-})
-module.exports = 'hello from b'
-
-},{"split-require":"split-require"}]},{},["entry3"]);
+},{}]},{},["entry3"]);
--- test/name/expected/f8da7c70ca.js
+++ test/name/expected/f8da7c70ca.js
@@ -1,13 +1,6 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"entry4":[function(require,module,exports){
 require("split-require").l(4, require("a"));
-},{"a":4,"split-require":"split-require"}],4:[function(require,module,exports){
-var splitRequire = require('split-require')
+},{"a":4,"split-require":1}],4:[function(require,module,exports){
+module.exports = 'this will also generate a hash'
 
-module.exports = function (cb) {
-  splitRequire(5, function (err, exports) {
-    if (err) cb(err)
-    else cb(null, 'hello from c: ' + exports)
-  })
-}
-
-},{"split-require":"split-require"}]},{},["entry4"]);
+},{}]},{},["entry4"]);
